0aa64ea3f472f7f216a39d483d88e42737246107
rubengaspar <46534187+rubengaspar@users.noreply.github.com>
2024-05-18 20:39:06 +0100

    updated ui

--- src/gui.py
+++ src/gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 from grid_manager import GridManager
 from robot import Robot
 from package import Package
@@ -6,57 +7,7 @@ from goal import Goal
 from pathfinding import Pathfinding
 
 class WarehouseGUI:
-    """WarehouseGUI class documentation
-
-    This class represents a graphical user interface (GUI) for a warehouse robot simulation. The GUI allows users to
-    interact with the simulation, such as adding robots and packages to the * warehouse grid, starting the
-    simulation, and resetting the grid.
-
-    Attributes:
-        - root (Tk): The root tkinter window of the GUI.
-        - grid_manager (GridManager): The grid manager object that handles the warehouse grid.
-        - current_action (str): The current action selected by the user (e.g., 'add_robot', 'add_package').
-        - highlight_rect (int): The ID of the highlighting rectangle on the canvas.
-        - cell_size (int): The size of each grid cell in pixels.
-
-    Methods:
-        - __init__(): Initializes the GUI and grid manager.
-        - create_widgets(): Creates and places the widgets in the GUI.
-        - prepare_add_robot(): Prepares to add a robot on the next canvas click.
-        - prepare_add_package(): Prepares to add a package on the next canvas click.
-        - on_canvas_click(event): Handles canvas click events.
-        - on_mouse_move(event): Handles mouse movement over the canvas to highlight grid cells.
-        - highlight_position(position): Highlights the grid cell at the given position.
-        - clear_highlight(): Clears the highlighted grid cell.
-        - reset(): Resets the warehouse grid.
-        - start(): Starts the simulation.
-        - stop(): Stops the simulation.
-        - update_simulation(): Updates the simulation by moving robots.
-        - update_canvas(): Updates the canvas with the current grid state.
-        - on_resize(event): Handles window resize events.
-        - run(): Runs the GUI main loop.
-
-    Example Usage:
-        gui = WarehouseGUI()
-        gui.run()
-    """
     def __init__(self):
-        """
-        Initialize the Warehouse Robot Simulation.
-
-        This method initializes the Tkinter root window, sets the title,
-        creates a GridManager object, sets the current action and
-        highlight_rect variables to None, and sets the default cell size.
-        It also creates widgets and binds the on_resize method to the
-        <Configure> event of the root window.
-
-        Parameters:
-            None
-
-        Returns:
-            None
-        """
-
         self.algorithm_choice = None
         self.option_menu = None
         self.canvas = None
@@ -74,15 +25,18 @@ class WarehouseGUI:
         self.root = tk.Tk()
         self.root.title("Warehouse Robot Simulation")
 
-        self.grid_manager = GridManager(width=15, height=15)
+        # Set the theme
+        self.style = ttk.Style(self.root)
+        self.style.theme_use('clam')
 
+        self.grid_manager = GridManager(width=15, height=15)
         self.pathfinder = Pathfinding(self.grid_manager)
         self.pathfinding_algorithm = 'a_star'
 
         self.current_action = None
         self.highlight_rect = None
 
-        self.cell_size = 0  # Default cell size
+        self.cell_size = 0
         self.simulation_running = False
 
         self.create_widgets()
@@ -90,48 +44,38 @@ class WarehouseGUI:
 
         self.robot_paths = {}
         self.counter = 0
+
     def create_widgets(self):
-        """
-        The create_widgets method is used to initialize and create the widgets for the GUI.
-        :return: None
-        """
-        self.canvas = tk.Canvas(self.root)
+        self.canvas = tk.Canvas(self.root, bg='white')
         self.canvas.grid(row=0, column=1, columnspan=3, sticky="nsew", padx=10, pady=10)
         self.canvas.bind("<Button-1>", self.on_canvas_click)
         self.canvas.bind("<Motion>", self.on_mouse_move)
 
         # Initialize a frame for buttons
-        self.left_frame = tk.Frame(self.root)
-        self.left_frame.grid(row=0, column=0, sticky="nsew")
+        self.left_frame = ttk.Frame(self.root)
+        self.left_frame.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
 
         # Add buttons to the frame
-        self.button_add_robot = tk.Button(self.left_frame, text="Add Robot", command=self.prepare_add_robot)
+        self.button_add_robot = ttk.Button(self.left_frame, text="Add Robot", command=self.prepare_add_robot)
         self.button_add_robot.pack(side="top", padx=5, pady=5)
 
-        self.button_add_package = tk.Button(self.left_frame, text="Add Package", command=self.prepare_add_package)
+        self.button_add_package = ttk.Button(self.left_frame, text="Add Package", command=self.prepare_add_package)
         self.button_add_package.pack(side="top", padx=5, pady=5)
 
-        self.button_select_goal = tk.Button(self.left_frame, text="Select Goal", command=self.prepare_add_goal)
+        self.button_select_goal = ttk.Button(self.left_frame, text="Select Goal", command=self.prepare_add_goal)
         self.button_select_goal.pack(side="top", padx=5, pady=5)
 
-        self.button_start = tk.Button(self.left_frame, text="Start", command=self.start_simulation)
+        self.button_start = ttk.Button(self.left_frame, text="Start", command=self.start_simulation)
         self.button_start.pack(side="top", padx=5, pady=5)
 
-        self.button_pause = tk.Button(self.left_frame, text="Pause", command=self.stop_simulation)
+        self.button_pause = ttk.Button(self.left_frame, text="Pause", command=self.stop_simulation)
         self.button_pause.pack(side="top", padx=5, pady=5)
 
-        self.button_reset = tk.Button(self.left_frame, text="Reset", command=self.reset)
+        self.button_reset = ttk.Button(self.left_frame, text="Reset", command=self.reset)
         self.button_reset.pack(side="top", padx=5, pady=5)
 
         self.algorithm_choice = tk.StringVar(value=self.pathfinding_algorithm)
-        self.option_menu = tk.OptionMenu(self.left_frame,
-                                         self.algorithm_choice,
-                                         'a_star',
-                                         'dijkstra',
-                                         'bfs',
-                                         'dfs',
-                                         'greedy_best_first',
-                                         command=self.set_algorithm)
+        self.option_menu = ttk.OptionMenu(self.left_frame, self.algorithm_choice, 'a_star', 'dijkstra', 'bfs', 'dfs', 'greedy_best_first', command=self.set_algorithm)
         self.option_menu.pack(side="top", padx=5, pady=5)
 
         self.root.grid_rowconfigure(0, weight=1)
@@ -142,40 +86,18 @@ class WarehouseGUI:
         self.pathfinding_algorithm = algorithm
 
     def prepare_add_robot(self):
-        """
-        Sets the current action as 'add_robot' and changes the cursor to a cross.
-
-        :return: None
-        """
         self.current_action = 'add_robot'
         self.root.config(cursor="cross")
 
     def prepare_add_package(self):
-        """
-        Sets the current action to 'add_package' and changes the cursor to a cross.
-
-        :return: None
-        """
         self.current_action = 'add_package'
         self.root.config(cursor="cross")
 
     def prepare_add_goal(self):
-        """
-        Sets the current action to 'add_goal' and changes the cursor to a cross.
-
-        :return: None
-        """
         self.current_action = 'add_goal'
         self.root.config(cursor="cross")
 
     def on_canvas_click(self, event):
-        """
-        Handle click event on the canvas.
-
-        :param event: The event object representing the click event.
-        :type event: tkinter.Event
-        :return: None
-        """
         x = (event.x - self.padding_x) // self.cell_size
         y = (event.y - self.padding_y) // self.cell_size
         position = (x, y)
@@ -196,15 +118,6 @@ class WarehouseGUI:
         self.update_canvas()
 
     def on_mouse_move(self, event):
-        """
-        :param event: The mouse event that triggered the method.
-        :return: None
-
-        This method is called when the mouse is moved over the specified area. It calculates the position of the
-        mouse cursor relative to the grid of cells, based on the event coordinates. It * then checks if the current
-        action is either "add_robot" or "add_package". If so, it highlights the position on the grid corresponding to
-        the calculated coordinates.
-        """
         x = (event.x - self.padding_x) // self.cell_size
         y = (event.y - self.padding_y) // self.cell_size
 
@@ -216,13 +129,6 @@ class WarehouseGUI:
             self.highlight_position((x, y), Goal.color)
 
     def highlight_position(self, position, color):
-        """
-        Highlight the given position on the canvas.
-
-        :param color:
-        :param position: The position to be highlighted. Should be a tuple in the format (x, y).
-        :return: None
-        """
         if self.highlight_rect:
             self.canvas.delete(self.highlight_rect)
         x, y = position
@@ -237,31 +143,15 @@ class WarehouseGUI:
             )
 
     def clear_highlight(self):
-        """
-        Clears the highlighting rectangle on the canvas.
-
-        :return: None
-        """
         if self.highlight_rect:
             self.canvas.delete(self.highlight_rect)
             self.highlight_rect = None
 
     def reset(self):
-        """
-        Reset method to reset the grid manager and update the canvas.
-
-        :return: None
-        """
         self.grid_manager.reset()
         self.update_canvas()
 
     def start_simulation(self):
-        """
-        Starts the simulation by setting the simulation_running flag to True
-        and calling the update_simulation method.
-
-        :return: None
-        """
         if (len(self.grid_manager.goals) > 0 and
                 len(self.grid_manager.packages) > 0 and
                 len(self.grid_manager.robots) > 0):
@@ -275,21 +165,10 @@ class WarehouseGUI:
             print(f"Number of Goals: {len(self.grid_manager.goals)}")
 
     def stop_simulation(self):
-        """
-        Stops the simulation by setting the simulation_running flag to False.
-
-        :return: None
-        """
         self.simulation_running = False
 
     def update_simulation(self):
-        """
-        Updates the simulation by moving robots. This method is called
-        periodically as long as the simulation is running.
-        :return: None
-        """
         if self.simulation_running:
-
             for robot in self.grid_manager.robots:
                 if not robot.path:
                     print(f"Calculating new path for robot {robot.id}")
@@ -312,14 +191,9 @@ class WarehouseGUI:
                 self.grid_manager.move_robots()
 
             self.update_canvas()
-            self.root.after(500, self.update_simulation)  # Update every 1 second
+            self.root.after(500, self.update_simulation)
 
     def update_canvas(self):
-        """
-        Update the canvas by redrawing the grid based on the grid manager's state.
-
-        :return: None
-        """
         self.canvas.delete("all")
         width = self.canvas.winfo_width()
         height = self.canvas.winfo_height()
@@ -330,7 +204,6 @@ class WarehouseGUI:
 
         for y in range(self.grid_manager.height):
             for x in range(self.grid_manager.width):
-
                 x1 = x * self.cell_size + self.padding_x
                 y1 = y * self.cell_size + self.padding_y
                 x2 = x1 + self.cell_size
@@ -344,27 +217,24 @@ class WarehouseGUI:
                         item_y2 = item_y1 + item_height
                         color = item.color if hasattr(item, 'color') else 'white'
                         self.canvas.create_rectangle(x1, item_y1, x2, item_y2, fill=color)
+                        # Draw the item ID as text on top of the rectangle
+                        if isinstance(item, Robot):
+                            self.canvas.create_text((x1 + x2) // 2, (item_y1 + item_y2) // 2,
+                                                    text=str(item.id)+","+str(len(item.packages)), fill="black")
+                        elif isinstance(item, Package):
+                            self.canvas.create_text((x1 + x2) // 2, (item_y1 + item_y2) // 2,
+                                                    text=str(item.id), fill="black")
+                        elif isinstance(item, Goal):
+                            self.canvas.create_text((x1 + x2) // 2, (item_y1 + item_y2) // 2,
+                                                    text=str(item.id) + "," + str(len(item.packages)), fill="black")
 
                 self.canvas.create_rectangle(x1, y1, x2, y2, outline="gray", width=1, stipple="gray12")
 
     def on_resize(self, event):
-        """
-        Handle the resize event.
-
-        :param event: The resize event object.
-        :type event: Event
-        :return: None
-        """
         self.update_canvas()
 
     def run(self):
-        """
-        Runs the main loop of the Tkinter application.
-
-        :return: None
-        """
         self.root.mainloop()
-
 
 if __name__ == "__main__":
     app = WarehouseGUI()
